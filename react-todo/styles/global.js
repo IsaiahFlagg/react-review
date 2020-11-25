1994a9b847526d84dd33288788bf1631b439aebc
I Flagg <isaiahnflagg@gmail.com>
2020-11-25 18:20:58 -0500

    global styles and fonts added.

---
+++ react-todo/styles/global.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        padding: 35
+    },
+    titleText: {
+        fontFamily: 'nunito-bold',
+        fontSize: 18,
+        color: '#333'
+    }
+});
