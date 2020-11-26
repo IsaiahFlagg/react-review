ff1d0020f9416f5beebd44877e7449322a1dbbd6
I Flagg <isaiahnflagg@gmail.com>
2020-11-25 19:33:15 -0500

    integrated stack navigation

--- react-todo/screens/home.js
+++ react-todo/screens/home.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
+import { globalStyles } from '../styles/global';
 
-export default function Home() {
+export default function Home({ navigation }) {
+    
+
+
+    const pressHandler = () => {
+        navigation.navigate('ReviewDetails')
+    }
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.titleText}>Home Screen</Text>
+        <View style={ globalStyles.container}>
+            <Text style={globalStyles.titleText}>Home Screen</Text>
+            <Button title='review details' onPress={pressHandler} />
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        padding: 35
-    },
-    titleText: {
-        fontFamily: 'nunito-bold',
-        fontSize: 18
-    }
-});
--- react-todo/styles/global.js
+++ react-todo/styles/global.js
@@ -2,11 +2,16 @@ import { StyleSheet } from 'react-native'
 
 export const globalStyles = StyleSheet.create({
     container: {
+        flex:1,
         padding: 35
     },
     titleText: {
         fontFamily: 'nunito-bold',
         fontSize: 18,
         color: '#333'
+    },
+    paragraph: {
+        marginVertical: 8,
+        lineHeight: 20
     }
 });
