869d65101b02189bbe041f013ea1bf7d712b5201
I Flagg <isaiahnflagg@gmail.com>
2020-11-25 19:59:23 -0500

    reviews section added to home

--- react-todo/screens/home.js
+++ react-todo/screens/home.js
@@ -1,19 +1,27 @@
-import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 import { globalStyles } from '../styles/global';
 
 export default function Home({ navigation }) {
-    
+    const [reviews, setReviews] = useState([
+        { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+        { title: 'Gotta Catch em All,', rating: 4, body: 'lorem ipsum', key: '2' },
+        { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' }
+    ]);
 
-
-    const pressHandler = () => {
-        navigation.navigate('ReviewDetails')
-    }
 
     return (
         <View style={ globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home Screen</Text>
-            <Button title='review details' onPress={pressHandler} />
+            <FlatList 
+                data={reviews}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+                        <Text style={globalStyles.titleText}>{item.title}</Text>
+                    </TouchableOpacity>
+                )}
+            />
+            
         </View>
     )
 }
--- react-todo/screens/reviewDetails.js
+++ react-todo/screens/reviewDetails.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }) {
+
+    
+
     return (
         <View style={globalStyles.container}>
             <Text>ReviewDetails Screen</Text>
+            
         </View>
     )
 }
