6614711d1ba70d6009ef87056b33b835420b08bd
I Flagg <isaiahnflagg@gmail.com>
2020-11-28 03:26:49 -0500

    form validation and custom button comp added

--- react-todo/screens/reviewForm.js
+++ react-todo/screens/reviewForm.js
@@ -2,6 +2,24 @@ import React from "react";
 import { StyleSheet, Button, TextInput, View, Text } from 'react-native';
 import { globalStyles } from '../styles/global.js';
 import { Form, Formik } from 'formik';
+import * as yup from 'yup';
+import FlatButton from '../shared/button'
+
+const reviewSchema = yup.object({
+    title: yup.string()
+        .required()
+        .min(4),
+    body: yup.string()
+        .required()
+        .min(8),
+    rating: yup.string()
+        .required()
+        .test('is-num-1-5', 'Rating must be a number 1 - 5', (val) => {
+            return parseInt(val) < 6 && parseInt(val) > 0;
+        })
+
+
+})
 
 export default function ReviewForm({ addReview }) {
 
@@ -10,6 +28,7 @@ export default function ReviewForm({ addReview }) {
         <View style={globalStyles.container}>
             <Formik
                 initialValues={{ title: '', body: '', rating: '' }}
+                validationSchema={reviewSchema}
                 onSubmit={(values, actions) => {
                     actions.resetForm();
                     addReview(values);
@@ -22,7 +41,9 @@ export default function ReviewForm({ addReview }) {
                             placeholder='Review title'
                             onChangeText={props.handleChange('title')}
                             value={props.values.title}
+                            onBlur={props.handleBlur('title')}
                         />
+                        <Text style={globalStyles.errorText}>{props.touched.title && props.errors.title}</Text>
 
                         <TextInput
                             multiline
@@ -30,7 +51,9 @@ export default function ReviewForm({ addReview }) {
                             placeholder='Review body'
                             onChangeText={props.handleChange('body')}
                             value={props.values.body}
+                            onBlur={props.handleBlur('body')}
                         />
+                        <Text style={globalStyles.errorText}>{props.touched.body && props.errors.body}</Text>
 
                         <TextInput
                             style={globalStyles.input}
@@ -38,9 +61,11 @@ export default function ReviewForm({ addReview }) {
                             onChangeText={props.handleChange('rating')}
                             value={props.values.rating}
                             keyboardType='numeric'
+                            onBlur={props.handleBlur('rating')}
                         />
+                        <Text style={globalStyles.errorText}>{props.touched.rating && props.errors.rating}</Text>
+                        <FlatButton text='submit' onPress={props.handleSubmit}/>
 
-                        <Button color='maroon' title="Submit" onPress={props.handleSubmit} />
                     </View>
 
                 )}
@@ -48,3 +73,5 @@ export default function ReviewForm({ addReview }) {
         </View>
     )
 }
+
+                    // <Button color='maroon' title="Submit" onPress={props.handleSubmit} />
